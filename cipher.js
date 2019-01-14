89978ffd4eba5ba6291891870500d38d49aa53c3
James Hooper <44674550+FitFingers@users.noreply.github.com>
2019-01-14 18:47:00 +0100

    Update cipher.js

--- cipher.js
+++ cipher.js
@@ -1,91 +1,61 @@
-window.onload = function() {
-  
-  // DEFINE VARIABLES
-  let offset = 1;
-  let caseSens = true;
-  
-  
-  
-  // DEFINE FUNCTIONS
-  // Encode message and put into outputBox
-  // BUG-FIX: Upper = -13)+65 (65 = "A"); Lower = -19)+97 (97 = "a"; difference of 6 due to the characters between ASCII "Z" and "a")
-  function cipher() {
-    // Avoid problems between blank and filled-in textarea
-    if (document.getElementById("inputText").value) {
-      var str = document.getElementById("inputText").value;
-    } else {
-      var str = document.getElementById("inputText").placeholder;
-    }
-    // Use input text to create output text
-    offset = parseInt(document.getElementById("incValue").innerHTML);
-    document.getElementById("outputText").innerHTML = str.replace(/[A-Z]/gi, upperOrLower);
-  }
+<head>
+    <title>Encrypt Your Messages and Retain Your Privacy | Caesar's Cipher</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="styles/cipher.css" rel="stylesheet" type="text/css">
+    <script src="scripts/cipher.js" type="text/javascript"></script>
+</head>
 
-  // Determine how to handle each character
-  function upperOrLower(letter) {
-    if (letter.charCodeAt(0) < 94) {
-      // Encode uppercase to uppercase
-      return String.fromCharCode((letter.charCodeAt(0) + 13 + offset) % 26 + 65);
-    } else {
-      if (caseSens == true) {
-        // Encode lowercase to lowercase
-        return String.fromCharCode((letter.charCodeAt(0) + 7 + offset) % 26 + 97);
-      } else {
-        // Encode lowercase to uppercase
-        return String.fromCharCode((letter.charCodeAt(0) + 7 + offset) % 26 + 65);
-      }
-    }
-  }
-  
-  // Copy contents to clipboard
-  function copyContent() {
-    window.prompt("Copy to clipboard: Ctrl+C, enter", document.getElementById("outputText").innerHTML);
-  }
-  
-  // Increment the offset value
-  function increment() {
-    offset++;
-    document.getElementById("incValue").innerHTML = offset;
-    cipher();
-  }
-  
-  // Decerement the offset value
-  function decrement() {
-    offset--;
-    document.getElementById("incValue").innerHTML = offset;
-    cipher();
-  }
-  
-  // Toggle case sensitive on/off
-  function toggleCaps() {
-    this.id == "caseYes" ? caseSens = true : caseSens = false;
-    cipher();
-  }
-  
-  
-  
-  // ADD EVENT LISTENERS
-  // Add convert button functionality
-  document.getElementById("encrypt").addEventListener("click", cipher);
-  
-  // Copy button
-  document.getElementById("copyButton").addEventListener("click", copyContent);
-  
-  // Increment button
-  document.getElementById("incre").addEventListener("click", increment);
-  
-  // Decrement button
-  document.getElementById("decre").addEventListener("click", decrement);
-  
-  // Caps toggle
-  document.getElementById("caseYes").addEventListener("click", toggleCaps);
-  document.getElementById("caseNo").addEventListener("click", toggleCaps);
-  
-  
-  
-  // FUNCTIONS TO RUN ON PAGE LOAD
-  // Run once on pageload for placeholder text
-  cipher();
-  
-  // END OF WINDOW.ONLOAD
-}
+<body>  
+  <header>
+    <h1>Javascript Caesar's Cipher</h1>
+    <h2>Encode and decode your messages</h2>
+  </header>
+
+  <main>
+    <section id="appContents">
+      
+  <!-- Input box -->
+      <div id="inputBox" class="mainContainer">
+        <textarea id="inputText" type="textarea" placeholder="Hello there, I am the Caesar Cipher. Please note the friendly message; this could easily have been an offensive one and you'd never have known!"></textarea>
+        <button id="runEncryption" class="button largebtn">Encrypt</button>
+      </div>
+
+  <!-- Settings box -->
+      <div id="settingsBox" class="mainContainer">
+        
+  <!-- Increment buttons -->
+        <span>Settings</span><hr>
+        <div class="optionContainer">
+          <div id="decrementOffset" class="option incrementer">
+            <div>⊖</div>
+          </div>
+          
+          <div id="valueDisplay" class="option">
+            <div id="offsetValue">1</div>
+          </div>
+          
+          <div id="incrementOffset" class="incrementer option">
+            <div>⊕</div>
+          </div>
+        </div>
+        
+  <!-- Caps toggle -->
+        <div id="caseSensitiveOptions">
+          <hr><span>Case sensitive:</span><hr>
+          <div class="optionContainer">
+            <div id="caseSensitiveOn" class="option">Yes</div>
+            <div id="caseSensitiveOff" class="option">No</div>
+          </div>
+        </div>
+      </div>
+
+  <!-- Output box -->
+      <div id="outputBox" class="mainContainer">
+        <textarea id="outputText" type="textarea"></textarea>
+        <button id="copyButton" class="button largebtn">Copy</button>
+      </div>
+      
+    </section>
+  </main>
+</body>
