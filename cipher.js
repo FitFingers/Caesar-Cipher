c76d46955f08e209663e67c83ecdee2d6d2ed2ae
James Hooper <44674550+FitFingers@users.noreply.github.com>
2019-01-14 18:15:54 +0100

    Update cipher.js

--- cipher.js
+++ cipher.js
@@ -22,18 +22,4 @@ window.onload = function() {
 
   // Run once on pageload for placeholder text
   cipher(document.getElementById("inputText").placeholder);
-  let alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
-  let inputSatz = document.getElementById("inputText").value;
-    
-  function cipher(str) {
-    // console.log("str is: " + str);
-    return str.toUpperCase().split("").map
-      (item => alpha.indexOf(item) !== -1 ? [alpha.indexOf(item) % 26 + 13].map
-        (item => alpha[item]) : item).join("");
-  }
-  
-  // document.getElementById("encrypt").addEventListener("click", cipher(x));
-  // document.getElementById("settingsBox").addEventListener("click", cipher(x));
-  
-  document.getElementById("outputText").innerHTML = cipher(inputSatz);
 }
